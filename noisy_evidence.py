ac9a389bae62023c4cba573870b8b4e86dc7ae14
Will Handley <wh260@cam.ac.uk>
2019-10-30 08:49:19 +0000

    Updated to also be able to do 2cosmo

--- noisy_evidence.py
+++ noisy_evidence.py
@@ -6,17 +6,24 @@ from DES import DES_like
 import camb
 from anesthetic import NestedSamples
 import sys
+import os
 
 
-DES_samples = NestedSamples(root='/rds/user/wh260/hpc-work/pablo/CosmoChord/runs/chains/DES')
-planck_samples = NestedSamples(root='/rds/user/wh260/hpc-work/pablo/CosmoChord/runs/chains/planck')
-DES_planck_samples = NestedSamples(root='/rds/user/wh260/hpc-work/pablo/CosmoChord/runs/chains/DES_planck')
+nested_dir = '/rds/user/wh260/hpc-work/pablo/CosmoChord/runs/chains/'
+DES_data = '/rds/user/wh260/hpc-work/pablo/CosmoChord/data/DES'
+
+#nested_dir = '/data/will/data/pablo/runs_default/chains'
+#DES_dir = '/home/will/Projects/CosmoChord/data/DES'
+
+DES_samples = NestedSamples(root=os.path.join(nested_dir,'DES'))
+planck_samples = NestedSamples(root=os.path.join(nested_dir,'planck'))
+DES_planck_samples = NestedSamples(root=os.path.join(nested_dir,'DES_planck'))
 
 # create a CosMOPED object
 TT2015 = CosMOPED('compression_vectors/output/LambdaCDM/', year=2015, spectra='TT', use_low_ell_bins=True)
 
 # create a DES object
-DESY1 = DES_like('/rds/user/wh260/hpc-work/pablo/CosmoChord/data/DES/DES_1YR_final.dataset') 
+DESY1 = DES_like(os.path.join(DES_dir,'DES_1YR_final.dataset')) 
 
 
 def random_posterior_sample(samples):
@@ -75,29 +82,42 @@ def loglike(H0, ombh2, omch2, tau, As, ns, DES=None, planck=None):
 planck_samples['1e-9A'] = 1e-9*planck_samples['A']
 DES_samples['1e-9A'] = 1e-9*DES_samples['A']
 params = ['H0', 'omegabh2', 'omegach2', 'tau', '1e-9A', 'ns']
-cov = planck_samples[params].cov()
-mu = planck_samples[params].mean()
-L = numpy.linalg.cholesky(cov)
-Linv = numpy.linalg.inv(L)
+planck_cov = planck_samples[params].cov()
+planck_mu = planck_samples[params].mean()
+planck_L = numpy.linalg.cholesky(planck_cov)
+planck_Linv = numpy.linalg.inv(planck_L)
+
+DES_cov = DES_samples[params].cov()
+DES_mu = DES_samples[params].mean()
+DES_L = numpy.linalg.cholesky(DES_cov)
+DES_Linv = numpy.linalg.inv(DES_L)
 
 from scipy.optimize import minimize
 #from camb import CAMBError
 
 if sys.argv[1] == '0':
-    theta = None
+    theta_planck = theta_DES = None
     planck = TT2015
     DES = DESY1
 else:
     numpy.random.seed(int(sys.argv[1]))
     if sys.argv[2] == 'planck':
-        theta = random_posterior_sample(planck_samples)
+        theta_planck = theta_DES = random_posterior_sample(planck_samples)
     elif sys.argv[2] == 'DES': 
-        theta = random_posterior_sample(DES_samples)
-    planck = planck_realization(*theta[params])
-    DES = DES_realization(*theta[params]) 
+        theta_planck = theta_DES = random_posterior_sample(DES_samples)
+    elif sys.argv[2] == '2cosmo':  
+        theta_planck = random_posterior_sample(planck_samples)
+        theta_DES = random_posterior_sample(DES_samples)
+
+    planck = planck_realization(*theta_planck[params])
+    DES = DES_realization(*theta_DES[params]) 
+
 
 def f(x, DES, planck):
-    t = mu + L @ x
+    if planck is None:
+        t = DES_mu + DES_L @ x
+    else:
+        t = planck_mu + planck_L @ x
     try:
         l = loglike(*t, DES=DES, planck=planck) 
         print(x,l)
@@ -107,23 +127,19 @@ def f(x, DES, planck):
 
 filename =  'data/' + sys.argv[2] + '_' + sys.argv[1] + '.txt'
 
-print('--------------------------------------------')
-with open(filename, "a") as myfile:
-    print('true theta:', file=myfile)
-
 sol = minimize(f,numpy.random.randn(6), args=(DES,planck),method='Nelder-Mead',options={'initial_simplex':numpy.random.randn(7,6)})
 with open(filename, "a") as myfile:
-    print('DES+planck theta:', numpy.array(mu + L @ sol.x), file=myfile)
+    print('DES+planck theta:', numpy.array(planck_mu + planck_L @ sol.x), file=myfile)
     print('DES+planck logL:', -sol.fun, file=myfile)
 
 print('--------------------------------------------')
 sol = minimize(f,numpy.random.randn(6), args=(None,planck),method='Nelder-Mead',options={'initial_simplex':numpy.random.randn(7,6)})
 with open(filename, "a") as myfile:
-    print('planck theta:', numpy.array(mu + L @ sol.x), file=myfile)
+    print('planck theta:', numpy.array(planck_mu + planck_L @ sol.x), file=myfile)
     print('planck logL:', -sol.fun, file=myfile)
 
 print('--------------------------------------------')
 sol = minimize(f,numpy.random.randn(6), args=(DES,None),method='Nelder-Mead',options={'initial_simplex':numpy.random.randn(7,6)})
 with open(filename, "a") as myfile:
-    print('DES theta:', numpy.array(mu + L @ sol.x), file=myfile)
+    print('DES theta:', numpy.array(DES_mu + DES_L @ sol.x), file=myfile)
     print('DES logL:', -sol.fun, file=myfile)
